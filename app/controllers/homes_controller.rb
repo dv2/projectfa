fb337da155b068ab8a53da7860d95c45d175e3d6
dvsuresh <dv.mymail@gmail.com>
2013-10-25 15:37:54 -0500

    add pie charts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,26 @@
 class HomesController < ApplicationController
   def index
+    @piechart_overall = Survey.overall
+
+    @piechart_gender_data_male = Survey.gender_data('Male')
+    @piechart_gender_data_female = Survey.gender_data('Female')
+
+
+    @piechart_district_data = []
+    @piechart_district_data << ['District', 'Votes']
+    @piechart_district_data << ['Hyderabad', 27]
+    @piechart_district_data << ['Vizag', 19]
+    @piechart_district_data << ['Krishna', 19]
+
+    @piechart_state_data = []
+    @piechart_state_data << ['State', 'Votes']
+    @piechart_state_data << ['Andhra Pradesh', 52]
+    @piechart_state_data << ['Tamil Nadu', 43]
+
+    @piechart_country_data = []
+    @piechart_country_data << ['Country', 'Votes']
+    @piechart_country_data << ['India', 232]
+    @piechart_country_data << ['United States', 29]
+    @piechart_country_data << ['China', 22]
   end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,33 @@
 class Survey < ActiveRecord::Base
   validates :pick, presence: true
+
+  def self.overall
+    out = []
+    out << ['x', 'y']
+    num = Survey.where(pick: 'United').count
+    out << ['United', num]
+    num = Survey.where(pick: 'Separate').count
+    out << ['Separate', num]
+    num = Survey.where(pick: 'None').count
+    out << ['Does Not Matter', num]
+
+    out
+  end
+
+  def self.gender_data(gender)
+    out = []
+    out << ['Male', 'Votes'] if gender == 'Male'
+    out << ['Female', 'Votes'] if gender == 'Female'
+
+    num = Survey.where(gender: gender, pick: 'United').count
+    out << ['United', num]
+
+    num = Survey.where(gender: gender, pick: 'Separate').count
+    out << ['Separate', num]
+
+    num = Survey.where(gender: gender, pick: 'None').count
+    out << ['Does Not Matter', num]
+
+    out
+  end
 end
