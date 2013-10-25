79e3380325d4d38c21e82c07d3d136aa94f3128a
dvsuresh <dv.mymail@gmail.com>
2013-10-25 13:13:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Futureandhra::Application.config.secret_key_base = 'ea5e6167145bbaa21671f249abd0528bca6c8e078b57a22d5141429faf5855e79a31672484edf773f05b30d529bdcc91232f1f99dc7a10e4d2ef678d8929ebbf'
