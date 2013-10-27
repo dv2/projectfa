05a07c56cf72ae8fc666e60a2d19d2ff4bb02539
dvsuresh <dv.mymail@gmail.com>
2013-10-26 23:36:36 -0500

    set user in survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,12 +1,13 @@
 class SurveysController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def new
     @survey = Survey.new
   end
 
   def create
     @survey = Survey.new(survey_params)
+    @survey.user = current_user
     if @survey.save
       flash[:success] = 'Thanks for submitting your choice.'
       redirect_to root_path
