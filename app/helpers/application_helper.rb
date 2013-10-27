5e4948ca74fd22d9b8f333231e84ff4346b7702e
dvsuresh <dv.mymail@gmail.com>
2013-10-27 16:49:38 -0500

    add logged in as in the top nav links, remove edit profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       false
     end
   end
+
+  def display_logged_in_info
+    if current_user.provider.nil?
+      "Logged in as #{current_user.email}"
+    elsif current_user.provider == 'facebook'
+      "Logged in via Facebook: #{current_user.p_nickname}"
+    end
+  end
 end
