64d2443379fd4c9855a803c6f8ac33101d679e69
dvsuresh <dv.mymail@gmail.com>
2013-10-30 07:00:51 -0500

    add methods to ApDistrict

--- app/models/ap_district.rb
+++ app/models/ap_district.rb
@@ -1,2 +1,13 @@
 class ApDistrict < ActiveRecord::Base
+  def self.telangana_districts
+    ["Adilabad", "Hyderabad", "Karimnagar", "Khammam", "Mahbubnagar", "Medak", "Nalgonda", "Nizamabad", "Rangareddy", "Warangal"]
+  end
+
+  def self.andhra_districts
+    ["Anantapur", "Chittoor", "East Godavari", "Guntur", "Cuddapah", "Krishna", "Kurnool", "Nellore", "Prakasam", "Srikakulam", "Vishakhapatnam", "Vizianagaram", "West Godavari"]
+  end
+
+  def self.andhra_and_telangana_districts
+    self.andhra_districts + self.telangana_districts
+  end
 end
